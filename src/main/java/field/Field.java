8b7111d5b7e538d48b4761b9ea0dcac2b1c6780b
Patrick <pbuller@deerplain.com>
2020-07-02 23:13:32 -0500

    Starting to stub out Field and Model base classes.

---
+++ src/main/java/field/Field.java
@@ -0,0 +1,7 @@
+package field;
+
+public class Field {
+    public Field() {
+        super();
+    }
+}
---
+++ src/main/java/model/Model.java
@@ -0,0 +1,24 @@
+package model;
+
+public class Model {
+    public Model() {
+        super();
+    }
+
+    public void save() {
+
+    }
+
+    public void getByPK(int pk) {
+
+    }
+
+    public void search(String field, String value) {
+
+    }
+
+    public void delete(int pk) {
+        
+    }
+    
+}
