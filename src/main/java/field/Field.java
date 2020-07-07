6adce39ac44c4e500c645e643cbbc0670c639887
Patrick <pbuller@deerplain.com>
2020-07-07 10:54:06 -0500

    Fleshing out Field base class.

--- src/main/java/field/Field.java
+++ src/main/java/field/Field.java
@@ -1,9 +1,23 @@
 package field;
 
 public class Field {
-    private String fieldname;
+    private String fieldName;
+    private String fieldType;
+    private boolean null_allowed = false;
     
     public Field() {
         super();
+    }
+    public Field(String fieldName, String fieldType, boolean null_allowed) {
+        this.fieldName = fieldName;
+        this.fieldType = fieldType;
+        this.null_allowed = null_allowed;
+    }
+    public String toString() {
+        if (null_allowed==true) {
+            return "    " + fieldName + " " + this.fieldType + " NOT NULL ";
+        } else {
+            return "    " + fieldName + " " + this.fieldType;
+        }
     }
 }
--- src/main/java/field/IntegerField.java
+++ src/main/java/field/IntegerField.java
@@ -3,5 +3,11 @@ package field;
 import field.IntegerField;
 
 public class IntegerField extends Field {
+    public IntegerField(String fieldName) {
+        super(fieldName, "INTEGER", false);
+    }
+    public IntegerField(String fieldName, boolean null_allowed) {
+        super(fieldName, "INTEGER", null_allowed);
+    }
     
 }
