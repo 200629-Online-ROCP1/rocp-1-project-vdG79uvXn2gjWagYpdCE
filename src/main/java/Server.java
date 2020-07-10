84e08ff0ae4fd71ddd28f9b56644b486cee923e0
Patrick <pbuller@deerplain.com>
2020-07-10 10:08:32 -0500

    Implemented and checked database connector.

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -2,36 +2,24 @@ import database.DBConnector;
 import logger.Logger;
 import model.*;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class Server {
 	
 	static public void main(String... args) {
-		DBConnector db = new DBConnector("172.18.0.2", "bank_database");
+		try {
+			Connection dbconn = DBConnector.getConnection("172.18.0.2", "bank_database");
+		} catch (SQLException e) {
+			System.out.println(e);
+		}
 
 		// Logger.setFilename("/tmp/bank_app.log");
 		// Logger.makeEntry("INFO", "Starting application");
 
-		db.connect();
-
-		// createSQL();
 		/* Status possibilities are Pending, Open, or Closed, or Denied */
-		AccountStatus accountStatus = new AccountStatus();
-		accountStatus.create("Pending");
-		accountStatus.save(db);
-
-		db.disconnect();
-	} 
-
-	static private void createSQL() {
-		Role Role = new Role();
-		AccountHolder AccountHolder = new AccountHolder();
-		AccountStatus AccountStatus = new AccountStatus();
-		AccountType AccountType = new AccountType();
-		Account Account = new Account();
-
-		System.out.println(Role.createSQL());
-		System.out.println(AccountHolder.createSQL());
-		System.out.println(AccountStatus.createSQL());
-		System.out.println(AccountType.createSQL());
-		System.out.println(Account.createSQL());
+		// AccountStatus accountStatus = new AccountStatus();
+		// accountStatus.create("Pending");
+		// accountStatus.save(db);
 	}
 }
--- src/main/java/database/DBConnector.java
+++ src/main/java/database/DBConnector.java
@@ -1,105 +1,53 @@
 package database;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import logger.Logger;
 
 public class DBConnector {
-	/* This will control connections to the database for the various models.
-	 * The username and password must exist as environment variables.
+	/** Utility class to create a connection to our database.
+	 *  The username and password must exist as environment variables.
 	 */
 
-	private String username = System.getenv("BANKDB_USERNAME");
-	private String password = System.getenv("BANKDB_PASSWORD");
+	private static String username = System.getenv("BANKDB_USERNAME");
+	private static String password = System.getenv("BANKDB_PASSWORD");
+	
+	// public int execute(String sql) {  // TODO remove
+	// 	// String query = "insert into emps (name, dept, salary) values (?,?,?)";
+	// 	// statement = con.prepareStatement(query,Statement.RETURN_GENERATED_KEYS);
+	// 	// statement.setString(1, "John");
+	// 	// statement.setString(2, "Acc Dept");
+	// 	// statement.setInt(3, 10000);
+	// 	// statement.executeUpdate();
+    //         // rs = pstmt.getGeneratedKeys();
+    //         // if(rs != null && rs.next()){
+    //         //     System.out.println("Generated Emp Id: "+rs.getInt(1));
+    //         // }
 
-	private int port = 5432; //Default port for postgres
-	private String hostname;
-	private String database;
+	// 	System.out.println("EXECUTING SQL => " + sql);
+	// 	try {
+	// 		Statement statement = dbconn.prepareStatement(sql,Statement.RETURN_GENERATED_KEYS);
+	// 		statement.setString(1, "Pending");
+	// 		statement.executeUpdate();
+	// 		ResultSet resultSet = statement.getGeneratedKeys();
+    //         while (resultSet.next()) {
+	// 			System.out.println(resultSet.getString("AccountStatus_id"));
+    //             System.out.println(resultSet.getString("status"));
+	// 		}
+	// 		resultSet.close();
+	// 		statement.close();
+	// 	} catch(SQLException se){
+	// 		se.printStackTrace();
+	// 	} catch(Exception e){
+	// 		e.printStackTrace();
+	// 	} finally {
+	// 		return 1;
+	// 	}
+	// }
 
-	public Connection dbconn;
-	public boolean is_connected = false;
-
-	public DBConnector() {
-		super();
-	}
-	public DBConnector(String hostname, int port, String database) {
-		super();
-		this.hostname = hostname;
-		this.port = port;
-		this.database = database;
-	}
-	public DBConnector(String hostname, String database) {
-		super();
-		this.hostname = hostname;
-		this.database = database;
-	}
-	public int execute(String sql) {
-		// String query = "insert into emps (name, dept, salary) values (?,?,?)";
-		// statement = con.prepareStatement(query,Statement.RETURN_GENERATED_KEYS);
-		// statement.setString(1, "John");
-		// statement.setString(2, "Acc Dept");
-		// statement.setInt(3, 10000);
-		// statement.executeUpdate();
-            // rs = pstmt.getGeneratedKeys();
-            // if(rs != null && rs.next()){
-            //     System.out.println("Generated Emp Id: "+rs.getInt(1));
-            // }
-
-		System.out.println("EXECUTING SQL => " + sql);
-		try {
-			Statement statement = dbconn.prepareStatement(sql,Statement.RETURN_GENERATED_KEYS);
-			statement.setString(1, "Pending");
-			statement.executeUpdate();
-			ResultSet resultSet = statement.getGeneratedKeys();
-            while (resultSet.next()) {
-				System.out.println(resultSet.getString("AccountStatus_id"));
-                System.out.println(resultSet.getString("status"));
-			}
-			resultSet.close();
-			statement.close();
-		} catch(SQLException se){
-			se.printStackTrace();
-		} catch(Exception e){
-			e.printStackTrace();
-		} finally {
-			return 1;
-		}
-	}
-
-	public void connect() {
-		/* Checks if there is currently a connection. If not,
-		 * tries to make connection. If it fails, logs the error
-		 * and returns false.
-		 */
-		if (hostname == null) {
-			Logger.makeEntry("ERROR", "No database server hostname provided.");
-		} else if (database == null) {
-			Logger.makeEntry("ERROR", "No database name provided.");
-		} else {
-			try {
-				String dbUrl = "jdbc:postgresql://" + hostname + ":" + port + "/" + database;
-				dbconn = DriverManager.getConnection(dbUrl, username, password);
-				Logger.makeEntry("INFO", "Connecting to the database.");
-				is_connected = true;
-			} catch (SQLException e) {
-				Logger.makeEntry("ERROR", "Database access failed " + e);
-			}
-
-		}
-	}
-	public void disconnect() {
-		/* Disconnects from the database. 
-		 */
-		try {
-			dbconn.close();
-			Logger.makeEntry("INFO", "Disconnecting from the database.");
-		} catch (SQLException e) {
-			Logger.makeEntry("ERROR", "Database disconnection failed " + e);
-		}
-		is_connected = true;
-	}
-
-	public void showCredentials() {
-		/* This is only for testing/debugging */
-		System.out.println(username + "/" + password);
+	public static Connection getConnection(String hostname, String database) throws SQLException {
+		String url = "jdbc:postgresql://" + hostname + ":5432/" + database;
+		return DriverManager.getConnection(url, username, password);
 	}
 }
