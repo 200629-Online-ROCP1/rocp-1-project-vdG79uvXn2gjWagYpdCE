9878cfa91343272e3b11424e115329d5e39d91a3
Patrick <pbuller@deerplain.com>
2020-07-13 21:58:07 -0500

    Have second method that reads database and hostname from env variables.

--- src/main/java/database/DBConnector.java
+++ src/main/java/database/DBConnector.java
@@ -12,7 +12,13 @@ public class DBConnector {
 
 	private static String username = System.getenv("BANKDB_USERNAME");
 	private static String password = System.getenv("BANKDB_PASSWORD");
+	private static String hostname = System.getenv("BANKDB_HOSTNAME");
+	private static String database = System.getenv("BANKDB_DATABASE");
 	
+	public static Connection getConnection() throws SQLException {
+		String url = "jdbc:postgresql://" + hostname + ":5432/" + database;
+		return DriverManager.getConnection(url, username, password);
+	}
 	public static Connection getConnection(String hostname, String database) throws SQLException {
 		String url = "jdbc:postgresql://" + hostname + ":5432/" + database;
 		return DriverManager.getConnection(url, username, password);
