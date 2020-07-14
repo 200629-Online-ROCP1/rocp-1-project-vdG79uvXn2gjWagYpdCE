d6576e713c11f9f24d9253ebc066fc417ce575eb
Patrick <pbuller@deerplain.com>
2020-07-13 23:23:48 -0500

    Starting to implement Account model.

---
+++ src/main/java/dao/AccountDAO.java
@@ -0,0 +1,102 @@
+package dao;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.sql.SQLException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import utils.Password;
+import model.AccountHolder;
+import database.DBConnector;
+
+public class AccountHolderDAO {
+    private static AccountHolderDAO self = new AccountHolderDAO();
+    
+    private AccountHolderDAO() {}
+	
+	public static AccountHolderDAO getInstance() {
+		return self;
+    }
+    
+    public boolean insert(AccountHolder accountHolder) {
+        try {
+            Connection dbconn = DBConnector.getConnection();
+			String sql = "INSERT INTO accountholder(username, email, firstname, lastname, password, role) VALUES(?, ?, ?, ?, ?, ?)";
+			PreparedStatement statement = dbconn.prepareStatement(sql);
+			statement.setString(1, accountHolder.getField("username"));
+			statement.setString(2, accountHolder.getField("email"));
+			statement.setString(3, accountHolder.getField("firstname"));
+			statement.setString(4, accountHolder.getField("lastname"));
+			statement.setString(5, Password.makeSHA256(accountHolder.getField("password")));
+			statement.setInt(6, accountHolder.getRoleID());
+
+			if(!statement.execute()) {
+				return true;
+			}
+		} catch (SQLException e) {
+			System.out.println(e);
+        }
+        return false;
+    }
+
+    public boolean update(AccountHolder accountHolder) {
+        try {
+			Connection dbconn = DBConnector.getConnection();
+			String sql = "UPDATE accountholder SET username=?, email=?, firstname=?, lastname=?, password=?, role=? WHERE accountholder_id=?";
+			PreparedStatement statement = dbconn.prepareStatement(sql);
+			statement.setString(1, accountHolder.getField("username"));
+			statement.setString(2, accountHolder.getField("email"));
+			statement.setString(3, accountHolder.getField("firstname"));
+			statement.setString(4, accountHolder.getField("lastname"));
+			statement.setString(5, Password.makeSHA256(accountHolder.getField("password")));
+			statement.setInt(6, accountHolder.getRoleID());
+            statement.setInt(7, accountHolder.getID());
+			
+			if(!statement.execute()) {
+				return true;
+			}
+		} catch (SQLException e) {
+			System.out.println(e);
+        }
+        return false;
+    }
+
+	public AccountHolder searchUsername(String username) {
+		try {
+			Connection dbconn = DBConnector.getConnection();
+			String sql = "SELECT * FROM accountholder WHERE username=?";
+            PreparedStatement statement = dbconn.prepareStatement(sql);
+			statement.setString(1, username);
+			ResultSet result = statement.executeQuery();
+			
+			if(result.next()) {
+				Map<String, String> data = new HashMap<String, String>();
+				data.put("role_id", String.valueOf(result.getInt("role")));
+				data.put("username", result.getString("username"));
+				data.put("firstname", result.getString("firstname"));
+				data.put("lastname", result.getString("lastname"));
+				data.put("password", result.getString("password"));
+				data.put("email", result.getString("email"));
+				return new AccountHolder(result.getInt("accountholder_id"), data);
+			}
+		} catch (SQLException e) {
+			System.out.println(e);
+		}
+        return null;
+
+	}
+
+    public void delete() {
+        try {
+            Connection dbconn = DBConnector.getConnection();
+			String sql = "DELETE FROM accountholder";
+            PreparedStatement statement = dbconn.prepareStatement(sql);
+            statement.execute();
+		}catch(SQLException e) {
+			System.out.println(e);
+		}
+    }
+}
--- src/main/java/model/Account.java
+++ src/main/java/model/Account.java
@@ -1,31 +1,107 @@
 package model;
 
+import dao.AccountHolderDAO;
+import database.DBConnector;
+import database.QueryBuilder;
 import field.*;
 
-/* public class Account {
-    private int accountId; // primary key
-    private double balance;  // not null
-    private AccountStatus status;
-    private AccountType type;
-    private boolean deleted;
-    private User owner;
-}
-*/
+import model.Role;
 
-public class Account extends Model {
-    DoubleField balance = new DoubleField("balance");
-    BooleanField deleted = new BooleanField("deleted");
-    ForeignKeyField account_status = new ForeignKeyField("AccountStatus");
-    ForeignKeyField account_type = new ForeignKeyField("AccountType");
-    ForeignKeyField owner = new ForeignKeyField("AccountHolder");
+import java.util.Map;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 
-    public Account() {
-        super("Account");
-        this.balance.Option("null_allowed", 0);
-        this.addField(balance);
-        this.addField(deleted);
-        this.addField(account_status);
-        this.addField(account_type);
-        this.addField(owner);
+public class AccountHolder {
+    private ArrayList<String> fields = 
+      new ArrayList<String>(Arrays.asList("username", "password", "firstname", "lastname", "email", "role"));
+    private Map<String, String> fieldValues = new HashMap<String, String>();
+    private Role role_fk;
+    private int primaryKey = 0;
+    private boolean saved = false;
+
+    // Constructors
+    public AccountHolder() {
+      super();
+    }
+
+    public AccountHolder(Map<String, String> data) {
+      super();
+      for (String field: fields) {
+        if (data.containsKey(field)) {
+          this.fieldValues.put(field, data.get(field));
+        } else {
+          if (field=="role") { // Missing the role, need to check if contains role_id
+            if (data.containsKey("role_id")) {
+              this.role_fk = Role.searchID(data.get("role_id"));
+            }
+          } else {
+            System.out.println("ERROR: No value was provided for field " + field);
+          }
+        }
+      }
+      this.role_fk = Role.search(data.get("role")); 
+    }
+    public AccountHolder(int pk, Map<String, String> data) { 
+      this(data);
+      this.primaryKey = pk;
+      this.saved = true;
+    }
+    public String toString() {
+      String retString = new String("PK => " + primaryKey + "\n");
+      for (String field: fields) {
+        if (field=="role") {
+          retString += "    Role PK => " + getRoleID() + " ("+ role_fk.getField("role") + ")\n";
+        } else {
+          retString += "    " + getField(field) + "\n";
+        }
+      }
+      if (!saved) {
+        retString += "    (NOT SAVED)";
+      }
+      return retString;
+
+    }
+
+    public String getField(String fieldName) {
+      return fieldValues.get(fieldName);
+    }
+    public void setField(Map<String, String> data) {
+      for (String field: data.keySet()) {
+        this.fieldValues.put(field, data.get(field));
+      }
+      saved = false;
+    }
+    public int getID() {
+      return primaryKey;
+    }
+    public int getRoleID() {
+      return role_fk.getID();
+    }
+
+    // Database operations - save(insert or update), search, refresh
+    public void save() {
+      AccountHolderDAO dao = AccountHolderDAO.getInstance(); 
+      if (primaryKey > 0) { //FIX
+        if (dao.update(this)) {
+          saved = true;
+        }
+      } else {
+        if (dao.insert(this)) {
+          AccountHolder tmp = AccountHolder.searchUsername(getField("username"));
+          this.primaryKey = tmp.primaryKey;
+          saved = true;
+        }
+      }
+    }
+
+    public static AccountHolder searchUsername(String username) {
+      AccountHolderDAO dao = AccountHolderDAO.getInstance(); 
+      return dao.searchUsername(username);
+    }
+
+    public static void deleteAll() {
+      AccountHolderDAO dao = AccountHolderDAO.getInstance(); 
+      dao.delete();
     }
 }
