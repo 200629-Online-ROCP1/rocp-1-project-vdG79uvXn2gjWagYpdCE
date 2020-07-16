f3a86c8ddd75160f6894c33fc45eee4e47172126
Patrick <pbuller@deerplain.com>
2020-07-15 23:28:23 -0500

    ERROR: Trying to build simpple connection to database, but getting jdbc error.

--- src/main/java/controller/AccountTypeAPI.java
+++ src/main/java/controller/AccountTypeAPI.java
@@ -1,10 +1,24 @@
 package controller;
 
+import java.util.Set;
+
 import javax.servlet.http.HttpServletRequest;
+
+import dao.AccountTypeDAO;
+import model.AccountType;
+
 import javax.servlet.http.HttpServletResponse;
 
 public class AccountTypeAPI {
 	public static String list(HttpServletRequest req, HttpServletResponse res) {
-		return "<h1>list from the account type api</h1>";
+//		Set<AccountType> types = AccountType.retrieveAll();
+//		String retString = "";
+//		for(AccountType type : types) {
+//			retString += type.toJSON();
+//		}
+//		return retString;
+		AccountType type = new AccountType("Testing");
+		type.save();
+		return type.toJSON();
 	}
 }
