e092382c10834f23f96e6454c8263e3ed770bbc7
Patrick <pbuller@deerplain.com>
2020-07-16 22:39:48 -0500

    Some cleanup

--- src/main/java/controller/AccountTypeAPI.java
+++ src/main/java/controller/AccountTypeAPI.java
@@ -1,20 +1,9 @@
 package controller;
 
 import java.util.ArrayList;
-import java.util.Set;
-
-import javax.servlet.http.HttpServletRequest;
-
-import dao.AccountTypeDAO;
 import model.AccountType;
 
-import javax.servlet.http.HttpServletResponse;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 public class AccountTypeAPI {
-	private static final ObjectMapper wrapper = new ObjectMapper();
 	
 	public static String list() {
 		ArrayList<AccountType> all = AccountType.retrieveAll();
