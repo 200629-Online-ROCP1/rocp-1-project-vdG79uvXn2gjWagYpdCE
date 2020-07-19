6dac70481321522aefd6a929bf02148fb3f71ff6
Patrick <pbuller@deerplain.com>
2020-07-19 00:24:29 -0500

    adding some data.

--- src/main/java/database/DBConnector.java
+++ src/main/java/database/DBConnector.java
@@ -3,6 +3,8 @@ package database;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+
+import dao.AccountDAO;
 import logger.Logger;
 
 public class DBConnector {
