6fb9ad1fe1d9a254debaf05dd2661034124ed01b
Patrick <pbuller@deerplain.com>
2020-07-21 20:03:46 -0500

    Getting rid of some old code.

--- src/main/java/field/IntegerField.java
+++
@@ -1,7 +0,0 @@
-package field;
-
-public class IntegerField extends Field {
-	public IntegerField(String fieldName) {
-		super(fieldName, "INTEGER");
-	}
-}
--- src/main/java/model/Model.java
+++ src/main/java/model/Model.java
@@ -1,62 +1,14 @@
 package model;
 
 import java.util.*;
-import field.*;
+
 
 public class Model {
 	private String tableName;
-	private ArrayList<field.Field> Fields = new ArrayList<field.Field>(12);
-	protected boolean is_saved = false;
-	protected int pk = 0;
+	protected boolean saved = false;
 
-	public Model(String tableName) {
+	public Model(String tablename) {
 		super();
 		this.tableName = tableName;
-		this.addPKField();
 	}
-
-	private void addPKField() {
-		PrimaryKeyField pk = new PrimaryKeyField(tableName + "_id");
-		Fields.add(pk);
-	}
-
-	protected void addField(field.Field newField) {
-		Fields.add(newField);
-	}
-
-	public String getPrimaryField() {
-		return Fields.get(0).FieldName();
-	}
-
-	public String createSQL() {
-		String retString = "DROP TABLE IF EXISTS " + tableName + ";\n";
-		retString += "CREATE TABLE " + tableName + "\n(\n";
-		for (int i = 0; i < Fields.size(); i++) {
-			retString += Fields.get(i);
-			if (i == Fields.size() - 1) {
-				retString += "\n";
-			} else {
-				retString += ",\n";
-			}
-		}
-		retString += ");\n\n";
-		return retString;
-	}
-
-	public void save() {
-
-	}
-
-	public void getByPK(int pk) {
-
-	}
-
-	public void search(String field, String value) {
-
-	}
-
-	public void delete(int pk) {
-
-	}
-
 }
